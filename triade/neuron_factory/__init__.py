061d2696cb84547641749fa14e7907ade7691ab8
WatabooColosus <168364521+WatabooColosus@users.noreply.github.com>
2026-07-11 19:58:31 -0500

    feat: expose sandbox execution engine

--- triade/neuron_factory/__init__.py
+++ triade/neuron_factory/__init__.py
@@ -1,6 +1,7 @@
 """Fábrica controlada de neuronas de Tríade Ω."""
 
 from .candidate import NeuronCandidate, NeuronCandidateFactory
+from .execution import SandboxExecutionEngine
 from .specification import (
     VALID_NEURON_STATES,
     VALID_TRANSITIONS,
@@ -16,6 +17,7 @@ __all__ = [
     "NeuronSpecification",
     "NeuronSpecificationStore",
     "ResourceBudget",
+    "SandboxExecutionEngine",
     "VALID_NEURON_STATES",
     "VALID_TRANSITIONS",
     "validate_transition",
