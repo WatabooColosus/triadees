60e9702cf51c9b9cb7f4c04fc0292622fd7a2e56
WatabooColosus <168364521+WatabooColosus@users.noreply.github.com>
2026-07-11 20:22:30 -0500

    feat: expose neuron lifecycle manager

--- triade/neuron_factory/__init__.py
+++ triade/neuron_factory/__init__.py
@@ -3,6 +3,7 @@
 from .candidate import NeuronCandidate, NeuronCandidateFactory
 from .evaluation import NeuronEvaluationCoordinator
 from .execution import SandboxExecutionEngine
+from .lifecycle import NeuronLifecycleManager
 from .specification import (
     VALID_NEURON_STATES,
     VALID_TRANSITIONS,
@@ -16,6 +17,7 @@ __all__ = [
     "NeuronCandidate",
     "NeuronCandidateFactory",
     "NeuronEvaluationCoordinator",
+    "NeuronLifecycleManager",
     "NeuronSpecification",
     "NeuronSpecificationStore",
     "ResourceBudget",
